1d0d4e5f741060c90ca74f147e9658c7b924c2d4
rapPayne <bob@creator.net>
2025-06-18 14:18:08 -0500

    Refactored to use .map()

--- vanillaClient/index.js
+++ vanillaClient/index.js
@@ -3,24 +3,28 @@ function pickPerson() {
   console.log('you clicked')
 }
 
+/**
+ * Makes a GET request for all the people and renders them when they arrive.
+ */
 async function fetchPeople() {
-  console.log('pressed me')
   const url = "http://localhost:3300/api/people"
-  // fetch(url)
-  //   .then(res => res.json())
-  //   .then(people => {
-  //     for (let person of people)
-  //       renderPerson(person)
-  //   })
   const people = await fetch(url).then(res => res.json())
-  for (let person of people)
-    renderPerson(person)
-
-  console.log("After the promise")
+  // Old way with a for-of loop:
+  // let html = '';
+  // for (let person of people)
+  //   html += makePersonSection(person)
+  // Refactored way with a .map() 🔥
+  let html = people.map(person => makePersonSection(person)).join('')
+  renderPersonSection(html)
 }
 
-function renderPerson(person) {
-  const html = `
+/**
+ * Creates the HTML to render for a person object
+ * @param {Person} person 
+ * @returns {string} The HTML 
+ */
+function makePersonSection(person) {
+  return `
       <section class="person-card">
         <img src="./assets/images/${person.first}.png" alt="${person.first}" />
         <h3>${person.first} ${person.last}</h3>
@@ -29,6 +33,17 @@ function renderPerson(person) {
         <p>${person.about}</p>
       </section>
   `
-  const wrapperSection = document.querySelector('#person-cards')
-  wrapperSection.innerHTML += html;
+}
+
+/**
+ * Crams the html in the section with an id of person-cards
+ * @param {string} html 
+ */
+function renderPersonSection(html) {
+  const wrapperSection = document.querySelector('#person-cards');
+  try {  // <-- In case the section wasn't found. (It's an error to refer to the innerHTML of a 'null')
+    wrapperSection.innerHTML = html;
+  } catch (e) {
+    console.error("No person cards section was found. Refresh the page.")
+  }
 }
